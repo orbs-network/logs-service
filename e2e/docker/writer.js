f8371230c9cb363f8af55a13830c051505f853f4
itamar Arjuan <itamar.arjuan@gmail.com>
2020-12-22 18:53:14 +0200

    adding missing console.log to avoid broken test

--- e2e/docker/writer.js
+++ e2e/docker/writer.js
@@ -3,6 +3,7 @@ require('express')()
         req.sBody = '';
         req.on('data', b => req.sBody += b);
         req.on('end', () => {
+            console.log(req.sBody);
             res.status(200).end();
         });
     })
