525639f33c3c33cce18ead67b93561e54b74281b
itamar Arjuan <itamar.arjuan@gmail.com>
2020-12-22 18:14:59 +0200

    adding a console log only for the healthcheck error handler

--- boyar/healthcheck.js
+++ boyar/healthcheck.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 try {
   const status = JSON.parse(fs.readFileSync('./status/status.json').toString());
   const updatedAgoSeconds = (new Date().getTime() - new Date(status.Timestamp).getTime()) / 1000;
-  if (updatedAgoSeconds > 15 * 60) {    
+  if (updatedAgoSeconds > 15 * 60) {
     process.exit(128);
   }
-} catch (err) {  
+} catch (err) {
+  console.log(err.stack);
   process.exit(0); // don't restart in this case, maybe service isn't ready
 }
 
